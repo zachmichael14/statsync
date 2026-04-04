732ee1ea0717eedefbe1e42e8183f57d8a28874f
Zach <zachmichael14@gmail.com>
2026-04-04 14:48:50 -0500

    Finish basic auth

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from urllib.parse import urlencode
 
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, redirect, request, session, url_for
 import requests
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ app = Flask(__name__)
 def index():
     params = {
         "client_id": os.getenv("CLIENT_ID"),
-        "redirect_uri": f"{os.getenv('DOMAIN_URI')}/home",
+        "redirect_uri": f"{os.getenv('DOMAIN_URI')}/auth",
         "response_type": "code",
         "scope": "activity:read_all,profile:read_all"
     }
@@ -28,11 +28,22 @@ def index():
         </html>
     '''
 
-@app.route("/home")
-def home():
-    return"<p>I'm a placeholder!</p>"
+@app.route("/auth")
+def auth():
+    auth_code = request.args.get("code")
+
+    request_body = {
+        "client_id": os.getenv("CLIENT_ID"),
+        "client_secret": os.getenv("CLIENT_SECRET"),
+        "code": auth_code,
+        "grant_type": "authorization_code"
+    }
+
+    response = requests.post(
+        "https://www.strava.com/oauth/token",
+        data=request_body
+    ).json()
 
 
-if __name__ == "__main__":
-    load_dotenv()
-    app.run()
+    session["access_token"] = response.get("access_token")
+    session["refresh_token"] = response.get("refresh_token")n   
